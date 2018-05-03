910c11b29e5cee378f285013269f1dd5314bb1c3
furixturi <alabebop@gmail.com>
2018-05-03 20:14:50 +0900

    Print UTF character with %q

--- 01-getting-started/02_numeral-systems/04_loop/main.go
+++ 01-getting-started/02_numeral-systems/04_loop/main.go
@@ -5,7 +5,7 @@ import (
 )
 
 func main() {
-	for i := 10000; i < 20000; i++ {
-		fmt.Printf("%d \t %b \t %x \n", i, i, i)
+	for i := 0; i < 200; i++ {
+		fmt.Printf("%d \t %b \t %x \t %q \n", i, i, i, i)
 	}
 }
