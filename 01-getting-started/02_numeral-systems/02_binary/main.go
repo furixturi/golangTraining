558d96eb81502bf19c06c99ff128eba21c7575ee
furixturi <alabebop@gmail.com>
2018-05-03 20:06:59 +0900

    Printf hexdecimal

--- 01-getting-started/02_numeral-systems/02_binary/main.go
+++ 01-getting-started/02_numeral-systems/02_binary/main.go
@@ -7,4 +7,10 @@ import (
 func main() {
 	// Print 42 in decimal, binary, hexdecimal
 	fmt.Printf("%d, %b, %x\n", 42, 42, 42)
+
+	//%x is to print hexdecimal with 0x in front
+	fmt.Printf("%d, %b, %#x\n", 42, 42, 42)
+
+	//%X is to print hexdecimal with 0X in front and capital A-F
+	fmt.Printf("%d, %b, %#X\n", 42, 42, 42)
 }
