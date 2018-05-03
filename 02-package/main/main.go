8cf9d5a9fc232a192ca327adff8dace0316bee2a
furixturi <alabebop@gmail.com>
2018-05-03 20:50:26 +0900

    package

---
+++ 02-package/main/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+
+	"stringutil"
+)
+
+func main() {
+	fmt.Println(stringutil.MyName)
+}
+cannot find package "stringutil" in any of:
+	/usr/local/go/src/stringutil (from $GOROOT)
+	/Users/alabebop/TuboDropbox/Dropbox/兔Dropbox/projects/go/src/stringutil (from $GOPATH)
